63c651f8227b13ced968232a7d1c0dc6f7e630eb
alex8023 <alex8023@yandex.ru>
2023-10-11 17:39:27 +0300

    add Layout to TimerPage

--- src/pages/TimerPage/TimerPage.tsx
+++ src/pages/TimerPage/TimerPage.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { TaskBlock } from '../../components/TaskBlock';
 import { TimerBlock } from '../../components/TimerBlock';
+import { Layout } from '../../components/Layout';
 
 export function TimerPage() {
   return (
-    <>
+    <Layout>
       <TimerBlock />
       <TaskBlock />
-    </>
+    </Layout>
   );
 }
